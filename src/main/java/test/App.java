20a90c4bedfaa40be02c1c4576484df069d29dd3
bernard <bernard@laurant.fr>
2022-08-22 12:07:26 +0200

    Initial commit

---
+++ src/main/java/test/App.java
@@ -0,0 +1,18 @@
+package test;
+
+
+import io.vavr.collection.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        List<Integer> list1 = List.of(1, 2, 3);
+        System.out.println(list1);
+    }
+}
